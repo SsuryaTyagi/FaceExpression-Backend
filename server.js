10579bb771e4991bd16a97677c6b45b16e5bbf40
SsuryaTyagi <2040surya@gmail.com>
2026-06-13 14:14:20 +0530

    server file

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const app = require("./src/App");
 const connectToDb = require("./src/config/database");
 
 const startServer = async () => {
-  await connectToDb(); 
+  await connectToDb();
 
-  app.listen(3000, () => {
-    console.log("http://localhost:3000");
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
   });
 };
 
